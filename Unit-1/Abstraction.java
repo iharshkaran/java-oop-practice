48444a23e4af260676a345c0d5e848697ca1b4e6
HARSH <harshkaranofficial@gmail.com>
2026-04-28 18:59:14 +0530

    OOPs

--- Unit-1/Abstraction.java
+++ Unit-1/Abstraction.java
@@ -1,19 +1,32 @@
-abstract class Animal{
-    abstract void sound(); // abstract method
-
-    void sleep(){ // Non-abstract method
-        System.out.println("Sleeping...");
-    }
-}
-class Dog extends Animal{
+// Abstract Class
+abstract class Animal {
     void sound(){
-        System.out.println("Barking");
+        System.out.println("Animal makes a sound");
+    }
+
+    abstract void eat(); // no body, must be implemented by subclasses
+}
+
+// Interface
+interface Pet {
+    void play(); // only method declaration, no body
+
+} 
+
+class Dog extends Animal implements Pet {
+    void eat() {
+        System.out.println("Dog eats dog food");
+    }
+
+    public void play() {
+        System.out.println("Dog plays with its owner");
     }
 }
 public class Abstraction {
     public static void main(String[] args){
-        Animal dog = new Dog();
-        dog.sound();
-        dog.sleep();
+        Dog d = new Dog();
+        d.sound(); // from abstract class Animal
+        d.eat(); // from abstract class Animal
+        d.play(); // from interface Pet
     }
 }

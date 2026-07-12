796a796d76eff79896188bd2c4323937913cd4d7
iharshkaran <harshkaranofficial@gmail.com>
2026-07-12 21:28:36 +0530

    Initialize Repo

--- README.md
+++
@@ -1,3 +0,0 @@
-# ☕ Java OOP Practice
-
-This repository contains my practice and learning of **Object Oriented Programming (OOP) using Java** along with core and advanced Java concepts.
--- Unit-1/StaticMember.java
+++
@@ -1,9 +0,0 @@
-class Student{
-    static String school = "ABC School"; //Static Member
-}
-public class StaticMember {
-    public static void main(String[] args){
-        Student s1 = new Student();
-        System.out.println(s1.school);
-    }
-}

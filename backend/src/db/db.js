ee4c1b7cce411c20bcf0975226e488cb616f8192
iharshkaran <harshkaranofficial@gmail.com>
2026-07-25 22:13:08 +0530

    feat: initialize backend

---
+++ backend/src/db/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+function connectToDB(){
+    mongoose.connect(process.env.MONGO_URL)
+    .then(()=>{
+        console.log("connected to DB");
+        
+    })
+}
+
+module.exports = connectToDB;
